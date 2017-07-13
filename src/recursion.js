684c1734789ded3b1d9f55070ff567d1b1d52100
Ciaran Martin Conners <cmc2295@columbia.edu>
2017-07-13 12:25:18 -0400

    Update recursion.js

--- src/recursion.js
+++ src/recursion.js
@@ -49,6 +49,12 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  }
+  return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
